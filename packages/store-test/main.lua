ba7534d1ba36c645441f1d4e34b263c3ce5c9d87
muserlogiccpu <developmentmuser@gmail.com>
2026-04-27 00:24:12 +0300

    Update main.lua

--- packages/store-test/main.lua
+++ packages/store-test/main.lua
@@ -1,7 +1,7 @@
 local function callback(frame, module)
 	local string = Instance.new("TextLabel")
 	string.Parent = frame
-  string.Name = "Never gonna give you up"
+  	string.Name = "Never gonna give you up"
 	string.Size = UDim2.new(1,0,1,0)
 	string.BackgroundTransparency = 1
 	string.Text = "Hello world!"
