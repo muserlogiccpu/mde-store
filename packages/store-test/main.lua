5deaaa2ea28115bd20b31d0773f8e7a8c5659154
muserlogiccpu <developmentmuser@gmail.com>
2026-04-27 00:24:00 +0300

    Create main.lua

---
+++ packages/store-test/main.lua
@@ -0,0 +1,10 @@
+local function callback(frame, module)
+	local string = Instance.new("TextLabel")
+	string.Parent = frame
+  string.Name = "Never gonna give you up"
+	string.Size = UDim2.new(1,0,1,0)
+	string.BackgroundTransparency = 1
+	string.Text = "Hello world!"
+  module.changeItemTheme(_G.darktheme, string)
+end
+return callback
